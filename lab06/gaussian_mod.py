4f07203d3f1dadb64442815fe6ac27862da22b52
EdozHub <edoard01@yahoo.it>
2025-04-16 15:04:44 +0200

    training e validation set

--- lab06/gaussian_mod.py
+++ lab06/gaussian_mod.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def mcol(x):
+def mrow(x):
     x = np.array(x)
     return x.reshape(1, x.shape[0])
 
-def mrow(y):
+def mcol(y):
     y = np.array(y)
     return y.reshape(y.shape[0], 1)
 
@@ -26,7 +26,18 @@ def read_file(filename):
             Llist.append(label)
     return np.hstack(Dlist), np.array(Llist)
 
+def split_db_2to1(D, L, seed=0):
+    nTrain = int(D.shape[1]*2/3)
+    np.random.seed(seed)
+    idx = np.random.permutation(D.shape[1])
+    idxTrain = idx[0:nTrain]
+    idxTest = idx[nTrain:]
+    DTR = D[:, idxTrain]
+    DTE = D[:, idxTest]
+    LTR = L[idxTrain]
+    LTE = L[idxTest]
+    return (DTR, LTR), (DTE, LTE)
+
 if __name__ == "__main__":
-    data, labels= read_file('iris.csv')
-    print(data)
-    print(labels)
+    data, labels= read_file('lab06/iris.csv')
+    (DTR, LTR), (DTE, LTE) = split_db_2to1(data, labels, seed=0)
