6341141dc77a31e61ab1e445c768276600458272
EdozHub <edoard01@yahoo.it>
2025-05-20 22:40:33 +0200

    estimation model

--- lab07/commedia.py
+++ lab07/commedia.py
@@ -1,4 +1,5 @@
 import load as ld
+import numpy
 
 def S1_createDictionary(tercets):
     sDict = set([]) # Vocabolario vuoto
@@ -8,15 +9,33 @@ def S1_createDictionary(tercets):
             sDict.add(word) # Aggiungo la parola al dizionario
     return sDict
 
-def S1_estimateModel(tercets, eps):
+def S1_estimateModel(hlTercets, eps):
     # Creo un vocabolario di tutte le parole presenti nel train set
     
     sDictCommon = set([]) # Vocabolario vuoto
-    for cls in tercets: 
-        ltercets = tercets[cls] # Ottengo le terzine di ciascuna cantica
+    for cls in hlTercets: 
+        ltercets = hlTercets[cls] # Ottengo le terzine di ciascuna cantica
         sDictCls = S1_createDictionary(ltercets) # Creo un vocabolario delle parole presenti nella cantica 
         sDictCommon = sDictCommon.union(sDictCls) # Aggiungo il vocabolario appena ottenuto a quello precedente
     
+    h_clsLogProb = {}
+    for cls in hlTercets:
+        h_clsLogProb[cls] = {w: eps for w in sDictCommon}
+    
+    for cls in hlTercets:
+        lTercets = hlTercets[cls]
+        for tercet in lTercets:
+            words = tercet.split()
+            for w in words:
+                h_clsLogProb[cls][w] += 1
+    
+    for cls in hlTercets:
+        nWordsCls = sum(h_clsLogProb[cls].values())  # Totale parole (con smoothing)
+        for w in h_clsLogProb[cls]:
+            h_clsLogProb[cls][w] = numpy.log(h_clsLogProb[cls][w]) - numpy.log(nWordsCls)
+
+    return h_clsLogProb
+
     
 
 
@@ -36,4 +55,3 @@ if __name__ == '__main__':
     ### SOLUZIONE 1 ###
 
     S1_model = S1_estimateModel(tercetsTrain, 0.001)
-    print(S1_model)
