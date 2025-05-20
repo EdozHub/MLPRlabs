6de7e9e12928c43c54944725eb909861a81e87d7
EdozHub <edoard01@yahoo.it>
2025-05-20 16:35:45 +0200

    creazione dei dizionari

--- lab07/commedia.py
+++ lab07/commedia.py
@@ -1,7 +1,16 @@
 import load as ld
+from collections import Counter
+from itertools import chain
+
+def createDictionary(data):
+    words = chain.from_iterable(map(str.split, data))
+    return Counter(words)
 
 if __name__ == '__main__':
     lInf, lPur, lPar = ld.load_data()
     trInf, valInf = ld.split_data(lInf, 4)
     trPur, valPur = ld.split_data(lPur, 4)
     trPar, valPar = ld.split_data(lPar, 4)
+    dicInf = createDictionary(trInf)
+    dicPur = createDictionary(trPur)
+    dicPar = createDictionary(trPar)
