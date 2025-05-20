14a755e8ea1589d5fc9cf0fa96cf549d9e3593d8
EdozHub <edoard01@yahoo.it>
2025-05-20 17:34:42 +0200

    inizio estimation

--- lab07/commedia.py
+++ lab07/commedia.py
@@ -1,16 +1,39 @@
 import load as ld
-from collections import Counter
-from itertools import chain
 
-def createDictionary(data):
-    words = chain.from_iterable(map(str.split, data))
-    return Counter(words)
+def S1_createDictionary(tercets):
+    sDict = set([]) # Vocabolario vuoto
+    for tercet in tercets: #Per ogni terzina
+        words = tercet.split()
+        for word in words: # Per ogni parola
+            sDict.add(word) # Aggiungo la parola al dizionario
+    return sDict
+
+def S1_estimateModel(tercets, eps):
+    # Creo un vocabolario di tutte le parole presenti nel train set
+    
+    sDictCommon = set([]) # Vocabolario vuoto
+    for cls in tercets: 
+        ltercets = tercets[cls] # Ottengo le terzine di ciascuna cantica
+        sDictCls = S1_createDictionary(ltercets) # Creo un vocabolario delle parole presenti nella cantica 
+        sDictCommon = sDictCommon.union(sDictCls) # Aggiungo il vocabolario appena ottenuto a quello precedente
+    
+    
+
 
 if __name__ == '__main__':
     lInf, lPur, lPar = ld.load_data()
     trInf, valInf = ld.split_data(lInf, 4)
     trPur, valPur = ld.split_data(lPur, 4)
     trPar, valPar = ld.split_data(lPar, 4)
-    dicInf = createDictionary(trInf)
-    dicPur = createDictionary(trPur)
-    dicPar = createDictionary(trPar)
+
+    chapterIndexes = {"inferno": 0, "purgatorio": 1, "paradiso": 2}
+    tercetsTrain = {
+        "inferno": trInf,
+        "purgatorio": trPur,
+        "paradiso": trPar
+    } 
+
+    ### SOLUZIONE 1 ###
+
+    S1_model = S1_estimateModel(tercetsTrain, 0.001)
+    print(S1_model)
